5d5b9b8b4a082556da432654809811ec932c647f
Tyler Morrison <tylermorrison7777@gmail.com>
2019-01-22 21:30:43 -0500

    Added Lab 2 and SPmatrix.
    Added class for matrices in SPmatrix. Added lab two test source code.

---
+++ include/SPmatrix.h
@@ -0,0 +1,46 @@
+#ifndef SMART_PRODUCTS_MATRIX
+#define SMART_PRODUCTS_MATRIX
+
+#include<vector>
+#include<math.h>
+#include<iostream>
+#include<stdlib.h>
+#include<stdexcept>
+#include<exception>
+
+/*
+	Namespace Smart Products (sp)
+*/
+namespace sp {
+	class MatF {
+
+	protected:
+		MatF_t mat_data_;
+		int n_rows_;
+		int n_cols_;
+		bool init_;
+
+	public:
+		// Constructors
+		MatF();
+		MatF(int,int,float);
+		MatF(const MatF_t);
+
+		// Getters
+		inline MatF_t getMatData() const;
+		inline int getNRows() const;
+		inline int getNCols() const;
+
+		// Self operations
+		MatF inv();
+		MatF t();
+
+		// Operators
+		MatF& operator= (const MatF&);
+		MatF operator+ (const MatF&);
+		MatF operator- (const MatF&);
+		MatF operator* (const MatF&);
+		friend std::ostream& operator<< (std::ostream&, const MatF&);	
+	};
+}
+#endif
