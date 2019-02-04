40a58cae18d180e33ec0f66f4c5f0468212cb9a5
Tyler Morrison <morrison.730@osu.edu>
2019-02-04 12:43:46 -0500

    Sync Lab 3 solution output.

---
+++ labs/3/lin/main.sol
@@ -0,0 +1,10 @@
+gpio_base_address is 0x3f200000
+pointer is 0x76f74000
+
+default pin set to 27
+Toggling Pin Mode ...
+toggle complete
+Writing Low : 
+Value of pin 27 is 0
+Writing High : 
+Value of pin 27 is 1
